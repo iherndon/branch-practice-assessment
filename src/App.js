6f3971744b9cefd81ffa3517bb0d2e23b9220437
Ivan <ivan@macbook-pro.lan>
2021-12-22 19:02:44 -0500

    Made first page of task list functional and styled correctly

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
+let num = 0;
+function generateId() {
+  return num++;
+}
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      text: "Complete Udemy course",
+      id: generateId(),
+      status: "Completed",
+    },
+    {
+      text: "Complete Egghead tutorials",
+      id: generateId(),
+      status: "In Progress",
+    },
+    {
+      text: "Do own research on JS Promises",
+      id: generateId(),
+      status: "Not Started",
+    },
+    {
+      text: "Complete Practice Skills Assessment",
+      id: generateId(),
+      status: "Not Started",
+    },
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: generateId(),
+        text: newTask,
+        status: "Not Started",
+      },
+    ]);
+
+    setNewTask("");
+  };
+
+  const handleChange = (evt) => setNewTask(evt.target.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <main>
         <h1>Tasks</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* represents table */}
+        <section>
+          <div className="row">
+            <h4>TASK</h4>
+            <h4>STATUS</h4>
+          </div>
+          {tasks.map((task) => (
+            <div className="row data">
+              <p>{task.text}</p>
+              <p>{task.status}</p>
+            </div>
+          ))}
+        </section>
+        <div className="row input-row">
+          <input type="text" value={newTask} onChange={handleChange} />
+          <button onClick={addTask} disabled={!newTask}>
+            Add
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
